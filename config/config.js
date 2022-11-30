9ace9d7dcee1a72790f387b04d8ebafd22fa677d
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    "Initial commit"

---
+++ config/config.js
@@ -0,0 +1,11 @@
+/**
+ * Config file: change the parameters according to your integration scenario
+ */
+
+module.exports = {
+  qlikWebIntegrationId: "your-web-id",
+  tenantHostname: "your-tenant-hostname",
+  appId: "your-app-id",
+  sheetId: "a8bdb8b2-525e-486e-91d1-7318d362acee",
+  theme: "embeddedtheme",
+};
