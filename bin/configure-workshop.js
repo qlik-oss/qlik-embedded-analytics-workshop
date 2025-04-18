205a894201abe477ac1099837d3600c9c0b51161
Jeff Goldberg <goldbergjeffrey@yahoo.com>
2025-04-18 13:37:02 -0400

    Jog/update access token code (#29)
    * new icon with logo
    
    * update with automated workshop setup start
    
    * update: includes creating oauth client
    
    * updated: added file manipulation to complete configuration.
    
    * update: added file creation and modification
    
    * update to support appId.
    
    * update devcontainer.json
    
    * Update devcontainer.json
    
    * Update devcontainer.json
    
    * updated code to use lambda function to get access token.

---
+++ bin/configure-workshop.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+import * as fs from 'fs';
+import * as path from 'path';
+import { dirname } from 'path';
+import { config } from 'process';
+const __dirname = dirname(new URL(import.meta.url).pathname);
+
+//data-host="https://0xnu8fry2fgj2i4.us.qlikcloud.com"
+
+// File path to the HTML file
+const filePath = path.join(__dirname, '../src/oauth-callback.html');
+
+function updateOAuthCallback(newHost) {
+
+  // Read the file
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the file:', err);
+      return;
+    }
+
+    // Replace the data-host parameter
+    const updatedData = data.replace(
+      /data-host="[^"]*"/,
+      `data-host="${newHost}"`
+    );
+
+    // Write the updated content back to the file
+    fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing to the file:', err);
+        return;
+      }
+    });
+  });
+
+}
+
+function createConfigFile(configData) {
+  const configFilePath = path.join(__dirname, '../config.js');
+  const words = `
+  const config = {
+    host: "${removeTrailingSlash(configData.host)}",
+    codespaceHostname: "${configData.codespaceHostname}",
+    clientId: "${configData.clientId}",
+    redirectUri: "${configData.codespaceHostname}oauth-callback.html",
+    appId: "${configData.appId}",
+    sheetId: "${configData.sheetId}"
+  }
+  export default config;
+  `
+  
+
+  fs.writeFileSync(configFilePath, words, 'utf-8');
+}
+
+function removeTrailingSlash(string) {
+  if (string && string.endsWith('/')) {
+    return string.slice(0, -1);
+  }
+  return string;
+}
+
+export { createConfigFile, updateOAuthCallback };
---
+++ bin/qlik-connect-tenant.js
@@ -0,0 +1,313 @@
+#!/usr/bin/env node
+
+import { input, confirm } from '@inquirer/prompts';
+import { triggerAsyncId } from 'async_hooks';
+import chalk from 'chalk';
+import { response } from 'express';
+import ora from 'ora';
+import validator from "email-validator";
+import { access } from 'fs';
+//import config from '../config.js.old';
+import randomstring from "randomstring";
+import { createConfigFile, updateOAuthCallback } from "./configure-workshop.js";
+
+const regionClientId = process.env["REGION_CLIENT_ID"];
+const regionClientSecret = process.env["REGION_CLIENT_SECRET"];
+
+const main = async () => {
+    let result = "";
+    const spinner = ora("");
+
+    const confirmParticipation = await confirm({
+        message: "Use of this workshop requires your name and email so that the tenant can create a user and send an email to you. Continue?",
+        default: true
+    });
+
+  if(!confirmParticipation) {
+    spinner.fail("You've decided not to participate. If you change your mind, run the script again.");
+    process.exit();
+  }
+
+  const userInput = {
+    email: await input({
+        message: "Enter your email address.",
+        validate: (email) => {
+            if(!validator.validate(email)) {
+                return "Please enter a valid email address."
+            }
+            return true
+        }
+    })
+  };
+
+  userInput.email = "jeffrey.p.goldberg+oem2025@gmail.com";
+
+  const createTenant = await confirm({
+    message: "Do you want to create a new tenant today?",
+    default: false
+  });
+
+  const tenant = await input({
+    message: 'Input the URL to your tenant. Currently must be a US region Qlik Cloud tenant only',
+  });
+
+    const tenantHostname = tenant;
+    const codespaceName = `https://${process.env["CODESPACE_NAME"]}-3000.app.github.dev/`;
+
+    const at = await getTenantAccessToken(tenantHostname, ["admin_classic"]);
+
+    //get appId for Sales Analytics app on tenant.
+    const appId = await getAppId(tenantHostname, at) || "NoAppFound";
+    if(appId) {
+        spinner.text = "The workshop app exists on the tenant"
+    }
+
+    spinner.start("Creating OAuth client on your tenant.")
+    const clientId = await createTenantOAuthClient(tenantHostname, codespaceName, at);
+    result = await sleep(1500, spinner, "Creating OAuth client on your tenant.");
+    if(clientId) {
+        spinner.succeed(`OAuth clientId created with value: ${clientId}`);
+    }
+
+    spinner.start("Updating oauth-callback file");
+    updateOAuthCallback(tenant);
+    sleep(1500, spinner, "Updating oauth-callback file");
+    spinner.succeed("oauth-callback file updated");
+
+    spinner.start("Creating config.js file");
+    sleep(1500, spinner, "Creating config.js file");
+    const configData = {
+        host: tenant,
+        codespaceHostname: codespaceName,
+        clientId: clientId.clientId,
+        appId: appId,
+        sheetId: "a8bdb8b2-525e-486e-91d1-7318d362acee"
+    };
+    createConfigFile(configData);
+    spinner.succeed("config.js file created");
+
+    spinner.start("Checking for email on tenant.");
+    const isUser = await userExists(tenantHostname, at, userInput);
+    
+    if(isUser) {
+        spinner.succeed("You have an account on this tenant.");
+    } else {
+        spinner.start("You need an account on this tenant. Let's create it.");
+        result = await sleep(1500, spinner, "Contacting your tenant");
+        spinner.succeed("Contacted tenant");
+        spinner.start("Creating user on the system");
+        const userCreated = await createUser(tenantHostname, at, userInput)
+        result = await sleep(1500, spinner, "Creating user on the system");
+        if(userCreated) {
+            const addAdminRole = await makeUserAdmin(tenantHostname, at, userCreated);
+            if(!addAdminRole) {
+                console.error("User not added as admin on tenant.")
+            }
+        } else {
+            throw new Error(`Workshop setup failed on user creation and invite on tenant`);
+        }
+        spinner.succeed("Created user on the system");
+        spinner.start("Check your email");
+        result = await sleep(1500, spinner, "Check your email");
+        spinner.succeed("Check your email");
+    }
+
+    spinner.succeed("You can now start the workshop by entering npm run start at the prompt");
+    process.exit();
+};
+
+main();
+
+async function getTenantAccessToken(tenantHostname, scopes) {
+
+    let lambdaURL = "https://ubzt66e9je.execute-api.us-east-1.amazonaws.com/"
+    //"https://ubzt66e9je.execute-api.us-east-1.amazonaws.com/token/"
+    const requestPayload = {
+        "tenantHostname": tenantHostname,
+        "scopes": scopes
+    };
+
+    //get access token to tenant
+    try {
+        const response = await fetch(`${lambdaURL}token/`, {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestPayload)
+        });
+        
+        if(!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+
+        const atData = await response.json();
+        console.log(atData);
+        return atData.accessToken;
+    } catch (error) {
+        console.error(`Failed to get access token: ${error}`);
+        throw new Error(`Failed to get access token: ${error}`);
+    }
+}
+
+async function createTenantOAuthClient(tenantHostname, codespaceHostname, accessToken) {
+   
+    const clientName = `Qlik-embed-workshop-${randomstring.generate(6)}`
+    const requestPayload = {
+        "appType": "spa",
+        "clientName": clientName,
+         "description": "The name of this client is " + clientName,
+         "redirectUris": [`${codespaceHostname}oauth-callback.html`],
+         "allowedScopes": ["user_default"],
+         "allowedOrigins": [`${removeTrailingSlash(codespaceHostname)}`]
+     };
+    
+    try {
+        const spaClient = await fetch(`${tenantHostname}api/v1/oauth-clients`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${accessToken}`
+            },
+            body: JSON.stringify(requestPayload)
+        });
+        const response = await spaClient.json();
+        return response.clientId;
+    } catch(error) {
+        throw new Error(`Failed to create OAuth client on ${tenantHostname}`, error)
+    }
+}
+
+async function createUser(tenantHostname, accessToken, userInput) {
+    
+    const requestPayload = {
+        invitees: [
+            {
+                email: userInput.email
+            }
+        ]
+    };
+    try {
+        const response = await fetch(`${tenantHostname}api/v1/users/actions/invite`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestPayload)
+    
+        });
+        
+        if(!response.ok) {
+            throw new Error(`Response status on invite user: ${response.status}`)
+        }
+
+        const data = await response.json();
+        const userId = data.data[0].userId;
+        return userId;
+    } catch (error) {
+        console.error(error.message);
+        throw new Error(`Failed to invite user to tenant.`);
+    }
+}
+
+async function makeUserAdmin(tenantHostname, accessToken, userId) {
+    const addRoleAssignment = [{
+        op: "replace",
+        path: "/assignedRoles",
+        value: [{
+            name: "TenantAdmin"
+        }]
+    }];
+
+    try {
+        const response = await fetch(`${tenantHostname}api/v1/users/${userId}`, {
+            method: "PATCH",
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(addRoleAssignment)
+        });
+    
+        if(!response.ok) {
+            console.error(`Response status on set role assignment on user: ${setUserAdmin.status}`);
+            return false;
+        }
+        return true;
+    } catch (error) {
+        console.error(error.message);
+            return false;
+    }
+}
+
+async function userExists(tenantHostname, accessToken, userInput) {
+    
+    const params = new URLSearchParams();
+    params.append("filter", `email eq "${userInput.email}"`);
+
+    try {
+        const response = await fetch(`${tenantHostname}api/v1/users?${params}`, {
+            headers: {
+                "Authorization": `Bearer ${accessToken}`
+            }
+        });
+        if(!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+        const data = await response.json();
+        if(data.data.length == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.error(error.message);
+        return false;
+    }
+
+}
+
+async function getAppId(tenantHostname, accessToken) {
+
+    const params = new URLSearchParams();
+    params.append("name", "sales analytics");
+    params.append("resourceType", "app");
+
+    try {
+        const response = await fetch(`${tenantHostname}api/v1/items?${params}`, {
+            headers: {
+                "Authorization": `Bearer ${accessToken}`
+            }
+        });
+        if(!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+        const data = await response.json();
+        if(data.data.length == 1) {
+            return data.data[0].resourceId;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.error(error.message);
+        return false;
+    }
+}
+
+function removeTrailingSlash(string) {
+    if (string && string.endsWith('/')) {
+      return string.slice(0, -1);
+    }
+    return string;
+  }
+
+async function sleep(ms, spinner, text) {
+    if(spinner && text) {
+        return new Promise(resolve => setTimeout(() => {
+            spinner.text = text;
+            resolve(text)
+        }, ms));
+    }
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
